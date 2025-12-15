d6d27f3aaadcde554374ac593d1f507e93c20d40
wei <wei@signerlabs.com>
2025-12-15 13:30:12 +0800

    Initial Commit

---
+++ ShipSwift/ShipSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShipSwiftApp.swift
+//  ShipSwift
+//
+//  Created by Wei on 2025/12/15.
+//
+
+import SwiftUI
+
+@main
+struct ShipSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
